da3627cdc323dc5b6a6ed68dbd35093a2cf2288e
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2022-01-24 23:47:25 -0600

    Delete runbuild.sh

--- runbuild.sh
+++
@@ -1,1 +0,0 @@
-nativefier https://openprocessing.org --name OpenProcessing --inject ./navbar.css --title-bar-style hidden --arch x64 --icon openprocessing.icns --platform mas --file-download-options '{"saveAs": true}' --internal-urls ".*?\.openprocessing\.*?"

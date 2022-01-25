21e7816b8088b99ac333b89ff251726574029f30
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2022-01-24 23:47:42 -0600

    Add files via upload

---
+++ buildmac.sh
@@ -0,0 +1,1 @@
+nativefier https://openprocessing.org --name OpenProcessing --inject ./navbar.css --title-bar-style hidden --arch x64 --icon openprocessing.icns --platform osx --file-download-options '{"saveAs": true}' --internal-urls ".*?\.openprocessing\.*?"
